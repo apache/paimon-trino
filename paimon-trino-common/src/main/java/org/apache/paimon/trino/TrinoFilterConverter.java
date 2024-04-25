64d32c8a566732372c8818d3837654fbba8e3f84
yilong.zyl <yilong.zyl@alibaba-inc.com>
2024-04-25 11:40:40 +0800

    support trino query paimon file index for map type with map-keys

--- paimon-trino-common/src/main/java/org/apache/paimon/trino/TrinoFilterConverter.java
+++ paimon-trino-common/src/main/java/org/apache/paimon/trino/TrinoFilterConverter.java
@@ -56,7 +56,6 @@ import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
-import java.util.regex.Pattern;
 
 import static io.trino.spi.type.TimeType.TIME_MILLIS;
 import static io.trino.spi.type.TimestampType.TIMESTAMP_MILLIS;
