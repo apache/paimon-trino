66ace420458acd0ef2e2f4f08a03705830c6ee1c
yejunhao <yejunhao.yjh@alibaba-inc.com>
2024-04-29 17:50:48 +0800

    fix comment

--- paimon-trino-common/src/main/java/org/apache/paimon/trino/TrinoConnectorFactory.java
+++ paimon-trino-common/src/main/java/org/apache/paimon/trino/TrinoConnectorFactory.java
@@ -19,29 +19,30 @@
 package org.apache.paimon.trino;
 
 import com.google.inject.Binder;
+import com.google.inject.Injector;
 import com.google.inject.Module;
+import io.airlift.bootstrap.Bootstrap;
+import io.airlift.json.JsonModule;
+import io.opentelemetry.api.OpenTelemetry;
+import io.opentelemetry.api.trace.Tracer;
+import io.trino.filesystem.manager.FileSystemModule;
+import io.trino.hdfs.HdfsModule;
+import io.trino.hdfs.authentication.HdfsAuthenticationModule;
+import io.trino.plugin.base.classloader.ClassLoaderSafeConnectorMetadata;
+import io.trino.plugin.base.classloader.ClassLoaderSafeConnectorPageSourceProvider;
+import io.trino.plugin.base.classloader.ClassLoaderSafeConnectorSplitManager;
+import io.trino.plugin.hive.NodeVersion;
+import io.trino.plugin.hive.orc.OrcReaderConfig;
+import io.trino.spi.classloader.ThreadContextClassLoader;
 import io.trino.spi.connector.Connector;
 import io.trino.spi.connector.ConnectorContext;
 import io.trino.spi.connector.ConnectorFactory;
+import io.trino.spi.type.TypeManager;
 
-import java.lang.reflect.InvocationTargetException;
 import java.util.Map;
-import java.util.Optional;
-
-import static org.apache.paimon.shade.guava30.com.google.common.base.Throwables.throwIfUnchecked;
 
 /** Trino {@link ConnectorFactory}. */
 public class TrinoConnectorFactory implements ConnectorFactory {
-
-    private final Class<? extends Module> module;
-
-    public TrinoConnectorFactory() {
-        this(EmptyModule.class);
-    }
-
-    public TrinoConnectorFactory(Class<? extends Module> module) {
-        this.module = module;
-    }
 
     @Override
     public String getName() {
@@ -51,27 +52,61 @@ public class TrinoConnectorFactory implements ConnectorFactory {
     @Override
     public Connector create(
             String catalogName, Map<String, String> config, ConnectorContext context) {
-        ClassLoader classLoader = context.duplicatePluginClassLoader();
-        try {
-            Class<?> moduleClass = classLoader.loadClass(Module.class.getName());
-            Object moduleInstance =
-                    classLoader.loadClass(module.getName()).getConstructor().newInstance();
-            return (Connector)
-                    classLoader
-                            .loadClass(InternalPaimonConnectorFactory.class.getName())
-                            .getMethod(
-                                    "createConnector",
-                                    Map.class,
-                                    ConnectorContext.class,
-                                    Optional.class,
-                                    moduleClass)
-                            .invoke(null, config, context, Optional.empty(), moduleInstance);
-        } catch (InvocationTargetException e) {
-            Throwable targetException = e.getTargetException();
-            throwIfUnchecked(targetException);
-            throw new RuntimeException(targetException);
-        } catch (ReflectiveOperationException e) {
-            throw new RuntimeException(e);
+        return create(catalogName, config, context, new EmptyModule());
+    }
+
+    public Connector create(
+            String catalogName,
+            Map<String, String> config,
+            ConnectorContext context,
+            Module module) {
+        ClassLoader classLoader = TrinoConnectorFactory.class.getClassLoader();
+        try (ThreadContextClassLoader ignored = new ThreadContextClassLoader(classLoader)) {
+            Bootstrap app =
+                    new Bootstrap(
+                            new JsonModule(),
+                            new TrinoModule(config),
+                            new HdfsModule(),
+                            new HdfsAuthenticationModule(),
+                            // bind the trino file system module
+                            new FileSystemModule(),
+                            binder -> {
+                                binder.bind(NodeVersion.class)
+                                        .toInstance(
+                                                new NodeVersion(
+                                                        context.getNodeManager()
+                                                                .getCurrentNode()
+                                                                .getVersion()));
+                                binder.bind(TypeManager.class).toInstance(context.getTypeManager());
+                                binder.bind(OpenTelemetry.class)
+                                        .toInstance(context.getOpenTelemetry());
+                                binder.bind(Tracer.class).toInstance(context.getTracer());
+                                binder.bind(OrcReaderConfig.class)
+                                        .toInstance(new OrcReaderConfig());
+                            },
+                            module);
+
+            Injector injector =
+                    app.doNotInitializeLogging()
+                            .setRequiredConfigurationProperties(Map.of())
+                            .setOptionalConfigurationProperties(config)
+                            .initialize();
+
+            TrinoMetadata trinoMetadata = injector.getInstance(TrinoMetadataFactory.class).create();
+            TrinoSplitManager trinoSplitManager = injector.getInstance(TrinoSplitManager.class);
+            TrinoPageSourceProvider trinoPageSourceProvider =
+                    injector.getInstance(TrinoPageSourceProvider.class);
+            TrinoSessionProperties trinoSessionProperties =
+                    injector.getInstance(TrinoSessionProperties.class);
+            TrinoTableOptions trinoTableOptions = injector.getInstance(TrinoTableOptions.class);
+
+            return new TrinoConnector(
+                    new ClassLoaderSafeConnectorMetadata(trinoMetadata, classLoader),
+                    new ClassLoaderSafeConnectorSplitManager(trinoSplitManager, classLoader),
+                    new ClassLoaderSafeConnectorPageSourceProvider(
+                            trinoPageSourceProvider, classLoader),
+                    trinoTableOptions,
+                    trinoSessionProperties);
         }
     }
 
