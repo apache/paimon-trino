17c94a820bdf55f316384ac49339cce2e20694e2
YeJunHao <41894543+leaves12138@users.noreply.github.com>
2024-11-27 15:01:29 +0800

    Flatten paimon-trino-440 to top level (#92)

---
+++ src/main/java/org/apache/paimon/trino/catalog/TrinoCatalog.java
@@ -0,0 +1,221 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.paimon.trino.catalog;
+
+import org.apache.paimon.catalog.Catalog;
+import org.apache.paimon.catalog.CatalogContext;
+import org.apache.paimon.catalog.CatalogFactory;
+import org.apache.paimon.catalog.Database;
+import org.apache.paimon.catalog.Identifier;
+import org.apache.paimon.fs.FileIO;
+import org.apache.paimon.fs.Path;
+import org.apache.paimon.manifest.PartitionEntry;
+import org.apache.paimon.options.Options;
+import org.apache.paimon.schema.Schema;
+import org.apache.paimon.schema.SchemaChange;
+import org.apache.paimon.security.SecurityContext;
+import org.apache.paimon.table.Table;
+import org.apache.paimon.trino.ClassLoaderUtils;
+import org.apache.paimon.trino.fileio.TrinoFileIOLoader;
+
+import io.trino.filesystem.TrinoFileSystem;
+import io.trino.filesystem.TrinoFileSystemFactory;
+import io.trino.spi.connector.ConnectorSession;
+import org.apache.hadoop.conf.Configuration;
+
+import java.util.List;
+import java.util.Map;
+
+/** Trino catalog, use it after set session. */
+public class TrinoCatalog implements Catalog {
+
+    private final Options options;
+
+    private final Configuration configuration;
+
+    private final TrinoFileSystemFactory trinoFileSystemFactory;
+
+    private Catalog current;
+
+    private volatile boolean inited = false;
+
+    public TrinoCatalog(
+            Options options,
+            Configuration configuration,
+            TrinoFileSystemFactory trinoFileSystemFactory) {
+        this.options = options;
+        this.configuration = configuration;
+        this.trinoFileSystemFactory = trinoFileSystemFactory;
+    }
+
+    public void initSession(ConnectorSession connectorSession) {
+        if (!inited) {
+            synchronized (this) {
+                if (!inited) {
+                    current =
+                            ClassLoaderUtils.runWithContextClassLoader(
+                                    () -> {
+                                        TrinoFileSystem trinoFileSystem =
+                                                trinoFileSystemFactory.create(connectorSession);
+                                        CatalogContext catalogContext =
+                                                CatalogContext.create(
+                                                        options,
+                                                        configuration,
+                                                        new TrinoFileIOLoader(trinoFileSystem),
+                                                        null);
+                                        try {
+                                            SecurityContext.install(catalogContext);
+                                        } catch (Exception e) {
+                                            throw new RuntimeException(e);
+                                        }
+                                        return CatalogFactory.createCatalog(catalogContext);
+                                    },
+                                    this.getClass().getClassLoader());
+                    inited = true;
+                }
+            }
+        }
+    }
+
+    @Override
+    public String warehouse() {
+        if (!inited) {
+            throw new RuntimeException("Not inited yet.");
+        }
+        return current.warehouse();
+    }
+
+    @Override
+    public Map<String, String> options() {
+        if (!inited) {
+            throw new RuntimeException("Not inited yet.");
+        }
+        return current.options();
+    }
+
+    @Override
+    public FileIO fileIO() {
+        if (!inited) {
+            throw new RuntimeException("Not inited yet.");
+        }
+        return current.fileIO();
+    }
+
+    @Override
+    public List<String> listDatabases() {
+        return current.listDatabases();
+    }
+
+    @Override
+    public void createDatabase(String s, boolean b, Map<String, String> map)
+            throws DatabaseAlreadyExistException {
+        current.createDatabase(s, b, map);
+    }
+
+    @Override
+    public Database getDatabase(String name) throws DatabaseNotExistException {
+        return current.getDatabase(name);
+    }
+
+    @Override
+    public void dropDatabase(String s, boolean b, boolean b1)
+            throws DatabaseNotExistException, DatabaseNotEmptyException {
+        current.dropDatabase(s, b, b1);
+    }
+
+    @Override
+    public Table getTable(Identifier identifier) throws TableNotExistException {
+        return current.getTable(identifier);
+    }
+
+    @Override
+    public Path getTableLocation(Identifier identifier) {
+        return current.getTableLocation(identifier);
+    }
+
+    @Override
+    public List<String> listTables(String s) throws DatabaseNotExistException {
+        return current.listTables(s);
+    }
+
+    @Override
+    public void dropTable(Identifier identifier, boolean b) throws TableNotExistException {
+        current.dropTable(identifier, b);
+    }
+
+    @Override
+    public void createTable(Identifier identifier, Schema schema, boolean ignoreIfExists)
+            throws TableAlreadyExistException, DatabaseNotExistException {
+        current.createTable(identifier, schema, ignoreIfExists);
+    }
+
+    @Override
+    public void renameTable(Identifier fromTable, Identifier toTable, boolean ignoreIfExistsb)
+            throws TableNotExistException, TableAlreadyExistException {
+        current.renameTable(fromTable, toTable, ignoreIfExistsb);
+    }
+
+    @Override
+    public void alterTable(Identifier identifier, List<SchemaChange> list, boolean ignoreIfExists)
+            throws TableNotExistException, ColumnAlreadyExistException, ColumnNotExistException {
+        current.alterTable(identifier, list, ignoreIfExists);
+    }
+
+    @Override
+    public void createPartition(Identifier identifier, Map<String, String> map)
+            throws TableNotExistException {
+        current.createPartition(identifier, map);
+    }
+
+    @Override
+    public void dropPartition(Identifier identifier, Map<String, String> partitions)
+            throws TableNotExistException, PartitionNotExistException {
+        current.dropPartition(identifier, partitions);
+    }
+
+    @Override
+    public List<PartitionEntry> listPartitions(Identifier identifier)
+            throws TableNotExistException {
+        return current.listPartitions(identifier);
+    }
+
+    @Override
+    public void close() throws Exception {
+        if (current != null) {
+            current.close();
+        }
+    }
+
+    @Override
+    public void createDatabase(String name, boolean ignoreIfExists)
+            throws DatabaseAlreadyExistException {
+        current.createDatabase(name, ignoreIfExists);
+    }
+
+    @Override
+    public void alterTable(Identifier identifier, SchemaChange change, boolean ignoreIfNotExists)
+            throws TableNotExistException, ColumnAlreadyExistException, ColumnNotExistException {
+        current.alterTable(identifier, change, ignoreIfNotExists);
+    }
+
+    @Override
+    public boolean allowUpperCase() {
+        return current.allowUpperCase();
+    }
+}
---
+++ src/test/java/org/apache/paimon/trino/TrinoTableHandleTest.java
@@ -0,0 +1,58 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.paimon.trino;
+
+import io.airlift.json.JsonCodec;
+import io.trino.spi.predicate.TupleDomain;
+import org.junit.jupiter.api.Test;
+
+import java.util.Collections;
+import java.util.Optional;
+import java.util.OptionalLong;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/** Test for {@link TrinoTableHandle}. */
+public class TrinoTableHandleTest {
+
+    private final JsonCodec<TrinoTableHandle> codec = JsonCodec.jsonCodec(TrinoTableHandle.class);
+
+    @Test
+    public void testPrestoTableHandle() throws Exception {
+        TrinoTableHandle expected =
+                new TrinoTableHandle(
+                        "test",
+                        "user",
+                        Collections.emptyMap(),
+                        TupleDomain.all(),
+                        Optional.empty(),
+                        OptionalLong.empty());
+        testRoundTrip(expected);
+    }
+
+    private void testRoundTrip(TrinoTableHandle expected) {
+        String json = codec.toJson(expected);
+        TrinoTableHandle actual = codec.fromJson(json);
+        assertThat(actual).isEqualTo(expected);
+        assertThat(actual.getSchemaName()).isEqualTo(expected.getSchemaName());
+        assertThat(actual.getTableName()).isEqualTo(expected.getTableName());
+        assertThat(actual.getFilter()).isEqualTo(expected.getFilter());
+        assertThat(actual.getProjectedColumns()).isEqualTo(expected.getProjectedColumns());
+    }
+}
